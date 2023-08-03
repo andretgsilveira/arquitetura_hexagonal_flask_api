d9cc480a7075dc4b02eef058675f96806d9229d6
andretgsilveira <andre_tgsilveira@hotmail.com>
2023-08-03 20:39:23 -0300

    Corrigido nome do arquivo de test

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import pytest
 
 if __name__ == '__main__':
 
+    pytest.main()
+
     file_handler = logging.FileHandler("app.log")
     file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
 
     logger = logging.getLogger("api")
     logger.addHandler(file_handler)
     logger.setLevel(logging.DEBUG)
-    pytest.main(["-v"])
+
     app = create_flask_app(logger)
     app.run(debug=True, host='0.0.0.0')
 
